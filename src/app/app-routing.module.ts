941c0ee2445cbc74553a7c46342320b1857a2603
Estefania Fajardo <a.fajardor@uniandes.edu.co>
2022-04-30 12:26:54 -0500

    App component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlantaListComponent } from './planta/planta-list/planta-list.component';
+import { APP_BASE_HREF } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'plantas', component: PlantaListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
 export class AppRoutingModule { }
